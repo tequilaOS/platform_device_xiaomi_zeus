d8ca7bcb2dc4e9038cbbf1238e0f499a6304a3aa
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    zeus: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -11,6 +11,16 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    FrameworksResZeus
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
