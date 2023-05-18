c79319a11d0568bb27b0bd9ec5800f9f96ecc418
Arian <arian.kulmer@web.de>
2023-05-18 23:19:07 +0200

    zeus: Initial proprietary-files.txt
    Change-Id: Ie3f4a882bc3d725192c20f875432368f15abf8a7

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/camera/zeus_enhance_motiontuning.xml|vendor/etc/camera/zeus_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        vendor/etc/camera/pureShot_parameter.xml|vendor/etc/camera/pureView_parameter.xml)
+            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
