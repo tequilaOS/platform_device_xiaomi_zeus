84b5f0d561de9672f24f9c6007a7d46e208f45ba
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-06-18 00:13:29 +0200

    zeus: Decommonize udfps long press sensor

--- device.mk
+++ device.mk
@@ -17,13 +17,6 @@ PRODUCT_PACKAGES += \
     SystemUIResZeus \
     WifiResZeus
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.xiaomi
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
